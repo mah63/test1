ba171ce3735a8a1567de5ecbf40f49b895b91171
mah63 <hagguimah@gmail.com>
2025-05-30 18:12:48 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Enable CORS
 app.use((req, res, next) => {
